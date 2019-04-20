e9d83d6d45c971536ac18b0d4db3e94b444c66d4
Tanuka Das <das.tanuka@yahoo.com>
2019-04-20 20:37:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,6 +12,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase)
+  # Check if string equals same phrase all upper cas
   if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   elsif phrase != phrase.upcase
