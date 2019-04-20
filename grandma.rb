8ec2fa994b94b9695049d52cd685a3d2fd0fbe6b
Tanuka Das <das.tanuka@yahoo.com>
2019-04-20 22:27:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -24,7 +24,13 @@ def speak_to_grandma(phrase)
 end
 end
 
-  
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase != phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  else
+    return "NO, NOT SINCE 1938!"
+
 
 
 =begin
