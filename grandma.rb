33bd95229e08fc45837570f0837c0ffaf134bf54
Tanuka Das <das.tanuka@yahoo.com>
2019-04-20 22:43:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -24,9 +24,11 @@ def speak_to_grandma(phrase)
 end
 =end
   if phrase = phrase.upcase
-    return "NO, NOT SINCE 1938!"
+    #return "NO, NOT SINCE 1938!"
     if phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
+    else
+      return "NO, NOT SINCE 1938!"
     end
   else
     return "HUH?! SPEAK UP, SONNY!"
