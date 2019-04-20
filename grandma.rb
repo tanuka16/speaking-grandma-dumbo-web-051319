8fdbd483e379339c9033f3b4167d96a2e73af8d3
Tanuka Das <das.tanuka@yahoo.com>
2019-04-20 22:32:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,8 +14,8 @@
 
 def speak_to_grandma(phrase)
   # Check if string equals same phrase all upper case
- if phrase phrase == "I LOVE YOU GRANDMA!" 
-    return "I LOVE YOU TOO PUMPKIN!"
+ if phrase phrase == "I LOVE YOU GRANDMA!"
+    return "NO, NOT SINCE 1938!" "I LOVE YOU TOO PUMPKIN!"
     if != phrase.upcase
       return "HUH?! SPEAK UP, SONNY!"
     end
