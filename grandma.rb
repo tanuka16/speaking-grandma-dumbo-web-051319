559bfa4d506f17a40c6e039ae69b859efb1b5bf0
Tanuka Das <das.tanuka@yahoo.com>
2019-04-20 22:47:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -23,6 +23,7 @@ def speak_to_grandma(phrase)
     return "NO, NOT SINCE 1938!"              #WHAT
 end
 =end
+
   if phrase = phrase.upcase
     #return "NO, NOT SINCE 1938!"
     if phrase == "I LOVE YOU GRANDMA!"
